a12bbd5cf1f6273cca975accd21201f1d88d11f6
Nick Liliakides <nick.liliakides@gmail.com>
2020-08-26 03:17:39 +0100

    apiKey moved to env var

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,13 +3,13 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: 'crwn-db.firebaseapp.com',
   databaseURL: 'https://crwn-db.firebaseio.com',
   projectId: 'crwn-db',
   storageBucket: 'crwn-db.appspot.com',
   messagingSenderId: '850995411664',
-  appId: '1:850995411664:web:7ddc01d597846f65'
+  appId: '1:850995411664:web:7ddc01d597846f65',
 };
 
 firebase.initializeApp(config);
@@ -29,7 +29,7 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
         displayName,
         email,
         createdAt,
-        ...additionalData
+        ...additionalData,
       });
     } catch (error) {
       console.log('error creating user', error.message);
